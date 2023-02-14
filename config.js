e3a6c4ceaf935b99560425d82fbfdf2bac5fe300
xoan3012 <108244100+xoan3012@users.noreply.github.com>
2023-02-14 14:20:18 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "My đẹt ti ni",
+    introTitle: 'Hê sờ lô hơ sờ ly ly',
+    introDesc: `<3`,
+    btnIntro: 'Go',
+    title: '5',
+    desc: `Em thích anh lắm đúng không <3, dám trả lời không đi`,
+    btnYes: 'Đúng rùi',
+    btnNo: 'Không nha :(((((((((',
+    question: 'Thôi cho em gõ câu trả lời đó',
+    btnReply: 'Gõ xong rồi thì kích zô',
+    reply: 'Em thích anh lắmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm',
+    mess: 'A biết mà',
+    messDesc: 'Lại bị anh thao túng rồi kk',
+    btnAccept: 'This is my heart',
+    messLink: '/page3.html'
+}
